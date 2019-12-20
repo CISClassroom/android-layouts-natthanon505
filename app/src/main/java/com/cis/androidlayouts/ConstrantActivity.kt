64795ad2b07dfbbc643be22ad6afb1b830b790b5
natthanon505 <33540480+natthanon505@users.noreply.github.com>
2019-12-20 12:04:08 +0700

    layoutlab

---
+++ app/src/main/java/com/cis/androidlayouts/ConstrantActivity.kt
@@ -0,0 +1,18 @@
+package com.cis.androidlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constrant.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ConstrantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrant)
+
+        button6.setOnClickListener {
+            this.finish()
+        }
+    }
+}
